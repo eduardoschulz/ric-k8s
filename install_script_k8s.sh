c4fdb5d631f3c656d360a69c4aa4f3f0ea46b824
Eduardo Schulz <eduardo.schulz@protonmail.com>
2024-06-26 16:14:16 -0300

    fix..

--- install_script_k8s.sh
+++ install_script_k8s.sh
@@ -29,14 +29,17 @@ sleep 2
 wget https://github.com/opencontainers/runc/releases/download/v1.1.13/runc.amd64
 sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 
+echo "Runc installed. Installing cni plugins"
+sleep 2
 #install cni plugins 
 wget https://github.com/containernetworking/plugins/releases/download/v1.5.1/cni-plugins-linux-amd64-v1.5.1.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.5.1.tgz
 
-echo "Runc installed. Installing cni plugins"
-sleep 2
 
+
+echo "Cni plugins installed. Installing kubeadm and kubelet"
+sleep 2
 #installing kubeadm
 curl -L "https://github.com/containernetworking/plugins/releases/download/v1.3.0/cni-plugins-linux-amd64-v1.3.0.tgz" | sudo tar -C /opt/cni/bin -xz
 curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.30.0/crictl-v1.30.0-linux-amd64.tar.gz" | sudo tar -C /usr/local/bin -xz
@@ -50,3 +53,8 @@ sudo mkdir -p /etc/systemd/system/kubelet.service.d
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/v0.16.2/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:/usr/local/bin:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 sudo systemctl daemon-reload && sudo systemctl enable --now kubelet
+echo "Kubeadm and Kubelet installed. Setting up system..."
+sleep 2
+
+sudo sed -i 's/\#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf #enable ipv4 forwarding
+sudo kubeadm init config.yaml
